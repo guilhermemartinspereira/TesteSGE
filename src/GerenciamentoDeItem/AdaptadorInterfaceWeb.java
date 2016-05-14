55bd9d6aa97567bdb73dbf94d582f6baacfaccc2
Diego Oliveira <diegopso2@gmail.com>
2016-05-13 22:42:30 -0300

    finalizacaode adaptador

--- src/GerenciamentoDeItem/AdaptadorInterfaceWeb.java
+++ src/GerenciamentoDeItem/AdaptadorInterfaceWeb.java
@@ -1,8 +1,5 @@
 package GerenciamentoDeItem;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,35 +8,30 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class AdaptadorInterfaceWeb implements Adaptador {
 
 	public static WebDriver driver;
-	public static String inicio = "https://www.google.com/";
-	public static Map<Estado, String> titulos;
-	public static Map<String, String> seletores;
+	public static ConhecimentoDeDominioDeInterfaceWeb conhecimento;
+	
 	static {
-		titulos = new HashMap<>();
-		titulos.put(Estado.CriandoItem, "SGE - Criação de Item");
-		titulos.put(Estado.ListandoItem, "SGE - Lista de Itens");
-		titulos.put(Estado.EditandoItem, "SGE - Edição de Item");
-		
-		seletores = new HashMap<>();
-		seletores.put("Botao Criar", ".jhp input[type='submit']");
-		seletores.put("Link de Item", ".item:nth-of-type(1)");
-		seletores.put("Botao Deletar", ".deletar:nth-of-type(1)");
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		driver = new ChromeDriver();
+		conhecimento = new ConhecimentoDeDominioDeInterfaceWeb();
+
+		try {
+			driver.get(conhecimento.get("url.inicio"));
+			driver.wait();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
 	public void executarEventoCriar(ContextoGerenciamentoDeItem contexto) {
 		// TODO Auto-generated method stub
 		try {
-			System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-			driver = new ChromeDriver();
+			driver.findElement(By.cssSelector(conhecimento.get("seletor.linkCriar"))).click();
+			driver.wait();
 			
-			driver.get(inicio);
-			Thread.sleep(2000);
-			
-			driver.findElement(By.cssSelector(seletores.get("Botao Criar"))).click();
-			Thread.sleep(2000);
-			
-			Assert.assertEquals("O título mudou para o da tela de criação? ", titulos.get(Estado.CriandoItem), driver.getTitle());
+			Assert.assertEquals("O título mudou para o da tela de criação? ", conhecimento.get("titulo." + Estado.CriandoItem.toString()), driver.getTitle());
 			
 			contexto.estado = Estado.CriandoItem;
 		} catch (Exception e) {
@@ -48,58 +40,150 @@ public class AdaptadorInterfaceWeb implements Adaptador {
 		}
 	}
 
+
 	@Override
 	public void executarEventoCancelarCriacao(ContextoGerenciamentoDeItem contexto) {
-		// TODO Auto-generated method stub
-
+		this.executarEventoCancelar(contexto);
 	}
 
 	@Override
 	public void executarEventoEditar(ContextoGerenciamentoDeItem contexto) {
 		// TODO Auto-generated method stub
-
+		try {
+			driver.findElement(By.cssSelector(conhecimento.get("seletor.linkEditar"))).click();
+			driver.wait();
+			
+			Assert.assertEquals("O título mudou para o da tela de edição? ", conhecimento.get("titulo." + Estado.EditandoItem.toString()), driver.getTitle());
+			
+			contexto.estado = Estado.EditandoItem;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void executarEventoCancelarEdicao(ContextoGerenciamentoDeItem contexto) {
-		// TODO Auto-generated method stub
-
+		this.executarEventoCancelar(contexto);
 	}
 
 	@Override
 	public void executarEventoCriarErro(ContextoGerenciamentoDeItem contexto) {
-		// TODO Auto-generated method stub
-
+		this.executarEventoSalvarDadosErro(contexto, Estado.CriandoItem, "mensagem.criarErro");
 	}
 
 	@Override
 	public void executarEventoCriarSucesso(ContextoGerenciamentoDeItem contexto) {
-		// TODO Auto-generated method stub
-
+		this.executarEventoSalvarDadosSucesso(contexto, "mensagem.criarSucesso");
 	}
 
 	@Override
 	public void executarEventoEditarErro(ContextoGerenciamentoDeItem contexto) {
-		// TODO Auto-generated method stub
-
+		this.executarEventoSalvarDadosErro(contexto, Estado.EditandoItem, "mensagem.editarErro");
 	}
 
 	@Override
 	public void executarEventoEditarSucesso(ContextoGerenciamentoDeItem contexto) {
-		// TODO Auto-generated method stub
-
+		this.executarEventoSalvarDadosSucesso(contexto, "mensagem.editarSucesso");
 	}
 
 	@Override
 	public void executarEventoBuscar(ContextoGerenciamentoDeItem contexto) {
-		// TODO Auto-generated method stub
-
+		try {
+			driver.findElement(By.cssSelector(conhecimento.get("seletor.campoConsulta")))
+				.sendKeys(conhecimento.get("dado.consulta"));
+			driver.findElement(By.cssSelector(conhecimento.get("seletor.linkPesquisar"))).click();
+			driver.wait();
+			
+			Assert.assertEquals("O título continuou igual? ", conhecimento.get("titulo." + Estado.ListandoItem), driver.getTitle());
+			Assert.assertTrue("Os resultados foram exibidos? ", driver.findElement(By.cssSelector(conhecimento.get("seletor.item"))).getText().contains(conhecimento.get("dado.consulta")));
+			
+			contexto.estado = Estado.ListandoItem;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void executarEventoDeletar(ContextoGerenciamentoDeItem contexto) {
-		// TODO Auto-generated method stub
+		try {
+			driver.findElement(By.cssSelector(conhecimento.get("seletor.linkDeletar"))).click();
+			driver.wait();
+			
+			Assert.assertEquals("O título continuou igual? ", conhecimento.get("titulo." + Estado.ListandoItem), driver.getTitle());
+			Assert.assertTrue("A mensagem de sucesso foi exibida? ", driver.getPageSource().contains(conhecimento.get(conhecimento.get("mensagem.deletarSucesso"))));
+			
+			contexto.estado = Estado.ListandoItem;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
+	private void executarEventoCancelar(ContextoGerenciamentoDeItem contexto) {
+		// TODO Auto-generated method stub
+		try {
+			driver.findElement(By.cssSelector(conhecimento.get("seletor.linkCancelar"))).click();
+			driver.wait();
+			
+			Assert.assertEquals("O título mudou para o da tela de listagem? ", conhecimento.get("titulo." + Estado.ListandoItem), driver.getTitle());
+			
+			contexto.estado = Estado.ListandoItem;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void executarEventoSalvarDadosErro(ContextoGerenciamentoDeItem contexto, Estado estado, String seletorMensagem) {
+		try {
+			Assert.assertFalse("Os dados são válidos?", contexto.dadosValidos.booleanValue());
+			
+			driver.findElement(By.cssSelector(conhecimento.get("seletor.campoNome")))
+				.sendKeys(conhecimento.get("dado.invalido.nome"));
+			driver.findElement(By.cssSelector(conhecimento.get("seletor.campoQuantidade")))
+				.sendKeys(conhecimento.get("dado.invalido.quantidade"));
+			driver.findElement(By.cssSelector(conhecimento.get("seletor.campoUnidade")))
+				.sendKeys(conhecimento.get("dado.valido.unidade"));
+			driver.findElement(By.cssSelector(conhecimento.get("seletor.campoPreco")))
+				.sendKeys(conhecimento.get("dado.valido.precoVenda"));
+			
+			driver.findElement(By.cssSelector(conhecimento.get("seletor.enviarDados"))).click();
+			driver.wait();
+			
+			Assert.assertEquals("O título continuou igual? ", conhecimento.get("titulo." + estado), driver.getTitle());
+			
+			Assert.assertFalse("A mensagem de erro foi exibida? ", !driver.getPageSource().contains(conhecimento.get(seletorMensagem)));
+			
+			contexto.estado = estado;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void executarEventoSalvarDadosSucesso(ContextoGerenciamentoDeItem contexto, String seletorMensagem) {
+		try {
+			Assert.assertFalse("Os dados são válidos?", contexto.dadosValidos.booleanValue());
+			
+			driver.findElement(By.cssSelector(conhecimento.get("seletor.campoNome")))
+				.sendKeys(conhecimento.get("dado.valido.nome"));
+			driver.findElement(By.cssSelector(conhecimento.get("seletor.campoQuantidade")))
+				.sendKeys(conhecimento.get("dado.valido.quantidade"));
+			driver.findElement(By.cssSelector(conhecimento.get("seletor.campoUnidade")))
+				.sendKeys(conhecimento.get("dado.valido.unidade"));
+			driver.findElement(By.cssSelector(conhecimento.get("seletor.campoPreco")))
+				.sendKeys(conhecimento.get("dado.valido.precoVenda"));
+			
+			driver.findElement(By.cssSelector(conhecimento.get("seletor.enviarDados"))).click();
+			driver.wait();
+			
+			Assert.assertEquals("O título mudou para o de listagem? ", conhecimento.get("titulo." + Estado.ListandoItem), driver.getTitle());
+			
+			Assert.assertFalse("A mensagem de erro foi exibida? ", !driver.getPageSource().contains(conhecimento.get(seletorMensagem)));
+			
+			contexto.estado = Estado.ListandoItem;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
---
+++ src/GerenciamentoDeItem/ConhecimentoDeDominioDeInterfaceWeb.java
@@ -0,0 +1,56 @@
+package GerenciamentoDeItem;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConhecimentoDeDominioDeInterfaceWeb {
+
+	public static String inicio = "https://www.google.com/";
+	public static Map<String, String> mapa;
+	
+	public ConhecimentoDeDominioDeInterfaceWeb() {
+		mapa = new HashMap<>();
+		
+		mapa.put("url.inicio", "SGE - Criação de Item");
+		
+		mapa.put("titulo." + Estado.CriandoItem.toString(), "SGE - Criação de Item");
+		mapa.put("titulo." + Estado.ListandoItem.toString(), "SGE - Lista de Itens");
+		mapa.put("titulo." + Estado.EditandoItem.toString(), "SGE - Edição de Item");
+		
+		mapa.put("seletor.linkCriar", "input[type='submit']");
+		mapa.put("seletor.linkEditar", ".item:nth-of-type(1)");
+		mapa.put("seletor.linkDeletar", ".deletar:nth-of-type(1)");
+		mapa.put("seletor.linkCancelar", ".cancelar");
+		mapa.put("seletor.linkPesquisar", ".pesquisar");
+		mapa.put("seletor.enviarDados", "input[type='submit']");
+		
+		mapa.put("seletor.campoNome", "input[name='nome']");
+		mapa.put("seletor.campoQuantidade", "input[name='quantidade']");
+		mapa.put("seletor.campoPreco", "input[name='preco']");
+		mapa.put("seletor.campoUnidade", "input[name='unidade']");
+		
+		mapa.put("seletor.campoConsulta", "input[name='consulta']");
+
+		mapa.put("mensagem.editarErro", "Erro ao Editar");
+		mapa.put("mensagem.criarErro", "Erro ao Criar");
+		mapa.put("mensagem.editarSucesso", "Editado com sucesso");
+		mapa.put("mensagem.criarSucesso", "Criado com sucesso");
+		mapa.put("mensagem.deletarSucesso", "Deletado com sucesso");
+		
+		mapa.put("dado.valido.nome", "Camiseta Florida");
+		mapa.put("dado.valido.quantidade", "50");
+		mapa.put("dado.valido.unidade", "UN");
+		mapa.put("dado.valido.precoVenda", "50,00");
+		
+		mapa.put("dado.invalido.nome", "");
+		mapa.put("dado.invalido.quantidade", "X");
+		mapa.put("dado.invalido.unidade", "");
+		mapa.put("dado.invalido.precoVenda", "X");
+		
+		mapa.put("dado.consulta", "camiseta");
+	}
+	
+	public String get(String chave) {
+		return mapa.get(chave);
+	}
+}
