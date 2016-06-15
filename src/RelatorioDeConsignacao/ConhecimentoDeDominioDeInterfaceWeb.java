c4472ba227f69dd73112071d76978bf9f95263d0
romulozn <romulozn@cpqd.com.br>
2016-06-15 16:42:34 -0300

    alteracao no teste

--- src/RelatorioDeConsignacao/ConhecimentoDeDominioDeInterfaceWeb.java
+++ src/RelatorioDeConsignacao/ConhecimentoDeDominioDeInterfaceWeb.java
@@ -10,10 +10,10 @@ public class ConhecimentoDeDominioDeInterfaceWeb {
 	public ConhecimentoDeDominioDeInterfaceWeb() {
 		mapa = new HashMap<>();
 		
-		mapa.put("url.inicio", "http://localhost:8100/#/tab/itens");
+		mapa.put("url.inicio", "file:///Users/romulozanconeto/Documents/Unicamp/Disciplines/2016-1sem-Engenharia%20de%20Software%20I%20-%20MO409A/Trabalho/Development/Interface/purplefitness-interface/consignacao_relatorio.html");
 			
 		mapa.put("seletor.linkGerarRelatorio", ".button-generate");
-		mapa.put("seletor.linkGerar", "#button-generate");
+		mapa.put("seletor.linkGerar", ".button-generate");
 		mapa.put("seletor.linkExportar", ".button-export");
 		mapa.put("seletor.linkCancelar", ".back-button");
 		mapa.put("seletor.enviarDados", ".button.button-full");
@@ -21,7 +21,7 @@ public class ConhecimentoDeDominioDeInterfaceWeb {
 		mapa.put("seletor.campoDataInicial", "input[name='data-inicial']");
 		mapa.put("seletor.campoDataFinal", "input[name='data-final']");
 
-		mapa.put("mensagem.sucesso", "Relatório exportado com sucesso");
+		mapa.put("mensagem.sucesso", "Relatorio exportado com sucesso");
 		
 		Date dataFinal = new Date(System.currentTimeMillis());
 		Date dataInicial = new Date(System.currentTimeMillis()-30*24*60*60*1000);
